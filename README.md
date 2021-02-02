4596bce033ed9fc84b50804cae1bfa12a412c308
Julian Gojani <julian@gojani.xyz>
2021-02-02 19:12:36 +0100

    Upgraded to APIv2 and added user support

--- README.md
+++ README.md
@@ -2,18 +2,6 @@
 
 ## Installation
 
-### Linux
-#### Using built version (amd)
-```bash
-sudo wget https://github.com/interaapps/pastefy-cli/releases/download/1.0/pastefy-linux-amd 
-sudo cp pastefy-linux-amd /usr/local/bin/pastefy
-sudo chmod +x /usr/local/bin/pastefy
-```
-##### or
-```bash
-wget -O - https://pastefy.ga/1QmBBpAE/raw | bash
-```
-#### Using non-built version
 `INFO` The pip modules requests, argparse python-packages are required
 ```bash
 git clone https://github.com/interaapps/pastefy-cli
@@ -21,18 +9,6 @@ cd pastefy-cli
 sudo cp main.py /usr/local/bin/pastefy
 # You may have to set the permission
 sudo chmod 777 /usr/local/bin/pastefy
-```
-
-### Windows
-Go to https://github.com/interaapps/pastefy-cli/releases and download the windows build. Unzip the folder (Rightclick, extract all), go into it, copy the path and add it into the path variables. (Search in windows-search for environment variables, click on the environment variables button and double-click on Path and add it.)
-
-### Compile it your own
-```bash
-pip install pyinstaller
-pyinstaller --hidden-import requests -n pastefy main.py
-# On linux
-pyinstaller --hidden-import requests --onefile -n pastefy main.py
-```
 
 ## Usage
 ```bash
@@ -47,4 +23,11 @@ pastefy -c "Hello world"
 
 # Paste contents with title
 pastefy -c "print('Hello')" -t "test.py"
+
+# Login with pastefy (https://pastefy.ga/apikeys)
+pastefy --key {api-key}
+
+pastefy --delete {paste_id}
+
+pastefy -f README.md --folder {folder_id}
 ```
--- main.py
+++ main.py
@@ -2,33 +2,64 @@
 
 import requests
 import argparse
+import json
+import os, os.path
+import pathlib
 
+configTemplate = {
+    "key": "--"
+}
 
-def paste(title, content):
-    response = requests.post("https://pastefy.ga/create:paste", json={
+config = configTemplate
+pathlib.Path(os.path.expanduser("~")+"/.config").mkdir(parents=True, exist_ok=True)
+configPath = os.path.expanduser("~")+"/.config/pastefycli.json"
+
+def paste(title, content, folder=""):
+    global config
+    response = requests.post("https://pastefy.ga/api/v2/paste", json={
         "content": content,
-        "title": title
+        "title": title,
+        "folder": folder
+    }, headers={
+        "x-auth-key": config["key"]
     })
 
     parsedResponse = response.json()
 
     if parsedResponse["success"]:
-        return parsedResponse["id"]
+        return parsedResponse["paste"]["id"]
     else:
         return False
 
+def writeConfig(content):
+    global config, configPath
+    with open(configPath, "w") as configFile:
+        config = {**configTemplate, **config, **content}
+        json.dump(config, configFile)
+
+
+
+if (not os.path.isfile(configPath)):
+    writeConfig(config)
+else:
+    with open(configPath, "r") as configFile:
+        config = json.load(configFile)
 
 if __name__ == '__main__':
     argParser = argparse.ArgumentParser(description="Pastefy CLI")
     argParser.add_argument("--file", "-f", type=str, help="Paste from file")
     argParser.add_argument("--title", "-t", type=str, help="Set Paste title")
     argParser.add_argument("--contents", "-c", type=str, help="Set Paste contents")
+    argParser.add_argument("--key", type=str, help="Set your api-key")
+    argParser.add_argument("--folder", type=str, help="Set folder")
+    argParser.add_argument("--delete", type=str, help="Deletes a paste")
 
     parsed = argParser.parse_args()
 
     pasteInformation = {
         "create": False,
         "title": "",
+        "folder": "",
         "content": None
     }
 
@@ -44,13 +75,41 @@ if __name__ == '__main__':
         pasteInformation["create"]  = True
         pasteInformation["content"] = parsed.contents
 
+    if parsed.folder is not None:
+        pasteInformation["folder"] = parsed.folder
+
     if pasteInformation["create"]:
         if pasteInformation["content"].strip() == "":
             print("Can't paste. Content is empty")
         else:
-            result = paste(pasteInformation["title"], pasteInformation["content"])
+            result = paste(pasteInformation["title"], pasteInformation["content"], pasteInformation["folder"])
 
             if isinstance(result, bool) and result is False:
                 print("Error during pasting")
             else:
                 print("https://pastefy.ga/"+result)
+    
+    if parsed.key is not None:
+        writeConfig({"key": parsed.key})
+        
+        response = requests.get("https://pastefy.ga/api/v2/user", headers={
+            "x-auth-key": config["key"]
+        })
+
+        parsedResponse = response.json()
+        if (parsedResponse["logged_in"]):
+            print("Welcome "+parsedResponse["name"]+"!")
+        else:
+            print("Couldn't log in")
+
+
+    if parsed.delete is not None:
+        response = requests.delete("https://pastefy.ga/api/v2/paste/"+parsed.delete, headers={
+            "x-auth-key": config["key"]
+        })
+
+        if (response.json()["success"]):
+            print("Deleted")
+        else:
+            print("Couldn't delete")
+    
