a269bbed4fe859cb9b207a52c53f26ced87a252a
Elvis Diniz <elvisdiniz@outlook.com>
2025-07-08 20:30:20 +0000

    refactor: reformat

--- main.py
+++ main.py
@@ -12,16 +12,20 @@ def main():
         cli = CLI(api, config)
 
         arg_parser = argparse.ArgumentParser(description="Pastefy CLI")
-        arg_parser.add_argument("--file", "-f", type=str, help="Paste from file")
-        arg_parser.add_argument("--title", "-t", type=str, help="Set Paste title")
+        arg_parser.add_argument(
+            "--file", "-f", type=str, help="Paste from file")
+        arg_parser.add_argument(
+            "--title", "-t", type=str, help="Set Paste title")
         arg_parser.add_argument("--contents", "-c", type=str,
                                 help="Set Paste contents")
         arg_parser.add_argument("--key", type=str, help="Set your api-key")
         arg_parser.add_argument("--base-url", type=str,
                                 help="Set your base url address")
         arg_parser.add_argument("--folder", type=str, help="Set folder")
-        arg_parser.add_argument("-d", "--delete", type=str, help="Deletes a paste")
-        arg_parser.add_argument("-y", "--yes", action="store_true", help="No interaction, assume yes for all prompts")
+        arg_parser.add_argument(
+            "-d", "--delete", type=str, help="Deletes a paste")
+        arg_parser.add_argument("-y", "--yes", action="store_true",
+                                help="No interaction, assume yes for all prompts")
 
         parsed_args = arg_parser.parse_args()
         cli.run(parsed_args)
