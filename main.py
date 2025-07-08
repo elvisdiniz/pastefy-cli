68f5bab7e37b1920d49f449e5e7f8e3cd1371971
Elvis Diniz <elvisdiniz@outlook.com>
2025-07-08 17:26:48 +0000

    refactor: reformat the code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import argparse
 from pastefy_cli import PastefyAPI, Config, CLI
 
+
 def main():
     config = Config()
     api = PastefyAPI(config)
@@ -11,14 +12,17 @@ def main():
     arg_parser = argparse.ArgumentParser(description="Pastefy CLI")
     arg_parser.add_argument("--file", "-f", type=str, help="Paste from file")
     arg_parser.add_argument("--title", "-t", type=str, help="Set Paste title")
-    arg_parser.add_argument("--contents", "-c", type=str, help="Set Paste contents")
+    arg_parser.add_argument("--contents", "-c", type=str,
+                            help="Set Paste contents")
     arg_parser.add_argument("--key", type=str, help="Set your api-key")
-    arg_parser.add_argument("--base-url", type=str, help="Set your base url address")
+    arg_parser.add_argument("--base-url", type=str,
+                            help="Set your base url address")
     arg_parser.add_argument("--folder", type=str, help="Set folder")
     arg_parser.add_argument("--delete", type=str, help="Deletes a paste")
 
     parsed_args = arg_parser.parse_args()
     cli.run(parsed_args)
+
 
 if __name__ == '__main__':
     main()
--- pastefy_cli.py
+++ pastefy_cli.py
@@ -4,6 +4,7 @@ import json
 import os
 import pathlib
 
+
 class PastefyAPI:
     def __init__(self, config):
         self.config = config
@@ -33,6 +34,7 @@ class PastefyAPI:
         )
         return response.json()
 
+
 class Config:
     def __init__(self, config_path=None):
         self.config_template = {
@@ -40,7 +42,8 @@ class Config:
             "baseUrl": "https://pastefy.ga",
             "apiVersion": "v2"
         }
-        self.config_path = config_path or os.path.expanduser("~/.config/pastefycli.json")
+        self.config_path = config_path or os.path.expanduser(
+            "~/.config/pastefycli.json")
         self.config = self.load_config()
 
     def load_config(self):
@@ -51,13 +54,15 @@ class Config:
             return {**self.config_template, **json.load(config_file)}
 
     def write_config(self, content):
-        pathlib.Path(os.path.dirname(self.config_path)).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(os.path.dirname(self.config_path)).mkdir(
+            parents=True, exist_ok=True)
         with open(self.config_path, "w") as config_file:
             self.config = {**self.config, **content}
             json.dump(self.config, config_file)
 
     def get(self, key):
         return self.config.get(key)
+
 
 class CLI:
     def __init__(self, api, config):
